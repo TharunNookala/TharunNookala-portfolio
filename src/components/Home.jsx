a29d9ebdcc5ffdcb081c938581557bdd7b488821
TharunNookala <iamtharun7799@gmail.com>
2024-01-20 15:15:44 +0530

    fix : home styles

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
     return (
         <HeroSection>
         <div name="home" className='h-full text-white py-4 px-4 flex flex-col'>
-            <div className='max-w-[1240px] h-[75vh] mx-auto grid md:grid-cols-2 sm:gap-4 items-center'>
+            <div className='max-w-[1240px] h-full sm:h-[75vh] mx-auto grid md:grid-cols-2 sm:gap-4 items-center'>
                <img className='w-[500px] mx-auto mt-[50px] md:mt-[30px] rounded-xl opacity-70 mix-blend-overlay hover:opacity-50' src={ProfileImage} alt='userphoto' />
                 <div className='md:mt-[100px]'>
                     <p className='font-bold p-2 text-2xl'>Hello! I'm</p>
@@ -16,7 +16,7 @@ const Home = () => {
                     </h1>
                     <p className='font-bold text-2xl'>A Frontend Developer <br /> Ex-Systems Engineer at Infosys</p>
                     <a href={Resume} download>
-                        <button className='text-[#ff7b9e] bg-white w-[200px] hover:bg-black hover:text-white rounded-md font-medium my-6 mx-auto md:mx-0 py-5'>
+                        <button className='text-black bg-white w-[200px] hover:bg-black hover:text-white rounded-md font-medium my-6 mx-auto md:mx-0 py-5'>
                             Download CV
                         </button>
                     </a>
